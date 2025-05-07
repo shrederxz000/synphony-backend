4564fc2dc2e0398f736996cfee455c5d4a00b971
shrederxz <gguukk88@gmail.com>
2025-05-07 06:56:08 +0300

    создал подключение к sqlite

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-app = FastAPI()
+engine = create_engine("sqlite:///mydatabase.db")
 
 
-@app.get('/root')
-def root():
-    return {'message': 'hello'}
+Session = sessionmaker(bind=engine)
+session = Session()
 
-if __name__ == "__main__":
-    import uvicorn 
-    uvicorn.run("main:app", reload=True)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    email = Column(String(100), unique=True)
+
+# Создание таблиц в БД
+Base.metadata.create_all(engine)
+
+
+# Добавление записи
+new_user = User(name="Anya", email="anyadura@example.com")
+session.add(new_user)
+session.commit()
+
+# Запрос данных
+user = session.query(User).filter_by(name="Alice").first
+print(user.id, user.name)
+
+session.close()  # важно закрывать сессию!
